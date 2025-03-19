d91a8cbcd1377108a32c090327551073b3e98326
Isaac <115880314+Isaacbarci@users.noreply.github.com>
2025-03-19 09:17:09 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Gera automaticamente uma versão do cache baseada no timestamp
-const CACHE_VERSION = '1742386625';
+// Gerar versão do cache baseada no timestamp APENAS UMA VEZ
+const CACHE_VERSION = `${Math.floor(Date.now() / 1000)}`;
 const CACHE_NAME = `my-site-cache-${CACHE_VERSION}`;
 
 const CACHE_FILES = [
