3c4749cf4d1a2c2aa3770aa12696328b0cfa7bb9
Isaac <115880314+Isaacbarci@users.noreply.github.com>
2025-04-05 10:36:05 -0300

    Delete build-service-worker.js

--- build-service-worker.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const commitHash = process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7) || 'dev';
-const template = fs.readFileSync('service-worker.template.js', 'utf8');
-const final = template.replace('{{COMMIT_HASH}}', commitHash);
-fs.writeFileSync('service-worker.js', final);
-console.log('Service Worker gerado com hash:', commitHash);
